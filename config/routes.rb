c235c0d41aeb03c1652b0b29cd95f829c4c6e239
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-04 02:20:07 +0000

    [Add]users/indexとレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
